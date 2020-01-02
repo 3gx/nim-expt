ff40b330420e96b22b90bc869ec5b238d2deb756
Evghenii <evghenii>
2020-01-02 09:54:15 -0800

    add some simple examples

---
+++ tut/greetings.nim
@@ -0,0 +1,21 @@
+# This is a commente
+block:
+  echo "What's your name? "
+  var name: string = readLine(stdin)
+  echo "Hi, ", name, "!"
+
+var x, y: int
+block:
+  var
+    x,y: int
+    # a comment can occur here to
+    a,b,c, : string
+
+const xc1 = "Abc"
+const
+    xc2 = 1
+    xy = 2
+    xz = xy + 5
+
+let xxx = x;
+
---
+++ tut/t3.nim
@@ -0,0 +1,19 @@
+echo "Conting to ten: "
+
+for i in countup(1,10):
+  echo i
+
+echo "Coutnign to 10:"
+var i = 1
+while i <= 10:
+  echo i
+  inc(i)
+
+for i in 1..10:
+  echo i
+
+for i in 0..<10:
+  echo i
+
+for index, item in ["a", "b"].pairs:
+  echo item, " at index ", index
