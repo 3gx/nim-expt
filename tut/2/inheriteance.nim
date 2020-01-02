50d9f6890514f0e32199627cbdfc41f981139398
Evghenii <evghenii>
2020-01-02 10:58:56 -0800

    add object conversion

--- tut/2/inheriteance.nim
+++ tut/2/inheriteance.nim
@@ -18,3 +18,11 @@ assert(person of Person)
 student = Student(name: "Anton", age: 5, id: 2)
 echo student[]
 
+person = Person(name: "Jack", age: 12)
+echo person[]
+
+proc getID(x: Person): int =
+  Student(x).id
+
+echo getID(student)
+# echo getID(person) # raises ObjectConvetionError exception
