09cde2d7cd12aeaeb71570ba461707fc46a15d55
Evghenii <evghenii>
2020-01-02 17:06:33 -0800

    add m5.nim

--- meta1/m5.nim
+++ meta1/m5.nim
@@ -1,4 +1,4 @@
-import json
+import json, macros
 
 var j2 =
   %[
@@ -13,3 +13,82 @@ var j2 =
   ]
 
 echo j2
+
+dumpLisp:
+  %[
+    %{
+      "name": %"John",
+      "age": %30
+    },
+    %{
+      "name": %"Susan",
+      "age": %31
+    }
+  ]
+
+dumpLisp:
+  Mfor
+
+var j1 = %*
+  [
+    {
+      "name": "John",
+      "age": 30
+    },
+    {
+      "name": "Susan",
+      "age": 31
+    }
+  ]
+
+echo j1
+dumpLisp:
+ %*
+  [
+    {
+      "name": "John",
+      "age": 30
+    },
+    {
+      "name": "Susan",
+      "age": 31
+    }
+  ]
+
+proc toJson(x: NimNode): NimNode {.compiletime.} =
+  case x.kind
+  of nnkBracket: # Corresponds to Bracket in dumpTree
+    result = newNimNode(nnkBracket)
+    for i in 0..<x.len:
+      result.add(toJson(x[i])) # Recurse to add %
+
+  of nnkTableConstr: # nnk stands for Nim node kind
+    result = newNimNode(nnkTableConstr)
+    for i in 0..<x.len:
+      assert x[i].kind == nnkExprColonExpr
+      result.add(newNimNode(nnkExprColonExpr)
+        .add(x[i][0])           # First element: no %
+        .add(toJson(x[i][1]))): # Second element: Recurse to add %
+
+  else:
+    result = x # End of recursion
+
+  result = result.prefix("%") # Surround this level with %
+
+macro `$*`*(x: untyped): untyped =
+  result = toJson(x)
+  echo result.repr
+
+var j3 = $*
+  [
+    {
+      "name": "John",
+      "age": 30
+    },
+    {
+      "name": "Susan",
+      "age": 31
+    }
+  ]
+echo j3
+
