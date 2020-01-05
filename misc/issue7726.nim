e19ab08ee5ab8dd0db53980bbec504e204c578f2
Evghenii <evghenii>
2020-01-04 18:09:47 -0800

    update issue7226.nim with mode code

--- misc/issue7726.nim
+++ misc/issue7726.nim
@@ -1,9 +1,20 @@
-import macros
+import macros, sugar
 
 macro foo(): untyped =
   let a = @[1, 2, 3, 4, 5]
+#  proc t(n:auto): auto = a[^n]
+#  let x = t 4
   result = quote do:
     `a.len`
 
 echo foo() # Outputs @[1, 2, 3, 4, 5] instead of 5
+
+proc f1(f: auto , n:int): int =
+  result = f(n,13)
+
+proc addx[T](n: T, m: T) : T =
+  result = m + n
+
+echo f1( (n:int,m:int) => m+n, 42)
+echo f1( addx[int], 42)
 
